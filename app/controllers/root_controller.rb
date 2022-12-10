74bd9bf3dd90910a63908a9558ce7ca3dde718eb
kermyndak <kermyndak@bk.ru>
2022-12-11 02:40:18 +0300

    First enabled version

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,18 +2,23 @@
 
 # This class - controller for work with pages input and show
 class RootController < ApplicationController
+  before_action :check, only: [:show]
   def input; end
 
   def show
-    if params[:query].nil?
-      redirect_to root_path, notice: 'Empty parameter'
-      return
+    if check_answer
+      @arr = Answer.find_by(input: @number).output
+                   .split(']]')
+                   .first.split('[[')[1..]
+                   .first.split('], [')
+                   .map { |pair| pair.split(', ').map(&:to_i) }
+    else
+      generate_arr
+      Answer.create(input: @number, output: @arr.to_s)
     end
-    redirect_to root_path, notice: 'Bad input...' unless params[:query].match?(/^[1-9]\d*$/)
-    @arr = []
-    @number = params[:query].to_i
-    generate_arr
   end
+
+  private
 
   def find(value)
     arr = [1]
@@ -24,6 +29,22 @@ class RootController < ApplicationController
       end
     end
     arr
+  end
+
+  def check
+    if params[:query].nil?
+      redirect_to root_path, notice: 'Empty parameter'
+      return
+    end
+    redirect_to root_path, notice: 'Bad input...' unless params[:query].match?(/^[1-9]\d*$/)
+    @arr = []
+    @number = params[:query].to_i
+  end
+
+  def check_answer
+    return false if Answer.find_by(input: @number).nil?
+
+    true
   end
 
   def generate_arr
