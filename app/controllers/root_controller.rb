ab36d16aa5791feb42d548226a99a9f8398f5ec8
kermyndak <kermyndak@bk.ru>
2022-12-11 01:48:07 +0300

    Added logic in project

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,34 @@
+# frozen_string_literal: true
+
+# This class - controller for work with pages input and show
 class RootController < ApplicationController
-  def input
-  end
+  def input; end
 
   def show
+    if params[:query].nil?
+      redirect_to root_path, notice: 'Empty parameter'
+      return
+    end
+    redirect_to root_path, notice: 'Bad input...' unless params[:query].match?(/^[1-9]\d*$/)
+    @arr = []
+    @number = params[:query].to_i
+    generate_arr
+  end
+
+  def find(value)
+    arr = [1]
+    (2..value**0.5).each do |i|
+      if (value % i).zero?
+        arr << i
+        arr << value / i unless i == value / i
+      end
+    end
+    arr
+  end
+
+  def generate_arr
+    @arr = (1..@number).map { |i| [i, find(i).sum] }
+                       .select { |val, sum| sum <= @number && val != sum && val == find(sum).sum }
+                       .uniq(&:min)
   end
 end
