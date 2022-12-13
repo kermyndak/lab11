5a469b481db3706b4b7791ecde5cef801dfb770f
kermyndak <kermyndak@bk.ru>
2022-12-13 04:40:13 +0300

    Perform functional

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,54 +2,22 @@
 
 # This class - controller for work with pages input and show
 class RootController < ApplicationController
-  before_action :check, only: [:show]
+  before_action :set_params, only: [:show]
   def input; end
 
   def show
-    if check_answer
-      @arr = Answer.find_by(input: @number).output
-                   .split(']]')
-                   .first.split('[[')[1..]
-                   .first.split('], [')
-                   .map { |pair| pair.split(', ').map(&:to_i) }
+    if @out.nil?
+      @arr = Answer.create(input: params[:query]).decode_output
+      flash[:notice] = 'Data download on DB'
     else
-      generate_arr
-      Answer.create(input: @number, output: @arr.to_s)
+      @arr = @out.decode_output
+      flash[:notice] = 'Data download of DB'
     end
   end
 
   private
 
-  def find(value)
-    arr = [1]
-    (2..value**0.5).each do |i|
-      if (value % i).zero?
-        arr << i
-        arr << value / i unless i == value / i
-      end
-    end
-    arr
-  end
-
-  def check
-    if params[:query].nil?
-      redirect_to root_path, notice: 'Empty parameter'
-      return
-    end
-    redirect_to root_path, notice: 'Bad input...' unless params[:query].match?(/^[1-9]\d*$/)
-    @arr = []
-    @number = params[:query].to_i
-  end
-
-  def check_answer
-    return false if Answer.find_by(input: @number).nil?
-
-    true
-  end
-
-  def generate_arr
-    @arr = (1..@number).map { |i| [i, find(i).sum] }
-                       .select { |val, sum| sum <= @number && val != sum && val == find(sum).sum }
-                       .uniq(&:min)
+  def set_params
+    @out = Answer.find_by(input: params[:query])
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,45 @@
 class Answer < ApplicationRecord
+  validates :input, {
+    presence: { message: 'Error, empty input value...' },
+    numericality: {
+      only_integer: true,
+      greater_than: 0,
+      message: 'Error, bad input, must be natural number'
+    }
+  }
+
+  before_create :set_params
+
+  def decode_output
+    ActiveSupport::JSON.decode(self.output)
+  end
+
+  private
+
+  def find(value)
+    arr = [1]
+    (2..value**0.5).each do |i|
+      if (value % i).zero?
+        arr << i
+        arr << value / i unless i == value / i
+      end
+    end
+    arr
+  end
+
+  def set_params
+    @arr = []
+    @number = self.input.to_i
+    self.output = encode_output
+  end
+
+  def encode_output
+    ActiveSupport::JSON.encode(generate_arr)
+  end
+
+  def generate_arr
+    (1..@number).map { |i| [i, find(i).sum] }
+                .select { |val, sum| sum <= @number && val != sum && val == find(sum).sum }
+                .uniq(&:min)
+  end
 end
