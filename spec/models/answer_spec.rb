a8077ed0d4e2211a5d7b2f204753afb04d22985a
kermyndak <kermyndak@bk.ru>
2022-12-17 05:52:07 +0300

    Added test for model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'If adding with similar params' do
+    before do
+      Answer.create!(input: 350) if Answer.find_by(input: 350).nil?
+    end
+
+    it 'should be error if input is not unique' do
+      expect { Answer.create!(input: 350) }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
+
+  describe 'If adding correct params' do
+    before do
+      Answer.delete_by(input: 350)
+    end
+    it 'Input: 350' do
+      expect(Answer.create(input: 350).output).to eq("[[220,284]]")
+    end
+  end
 end
